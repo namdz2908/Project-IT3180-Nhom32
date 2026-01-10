f9a45ef07112c242d7f47dc75a920c3d2a54e3fc
Đào Quang <anhdaoktq@gmail.com>
2026-01-10 15:45:58 +0700

    Thêm local host

--- backend/src/main/java/com/prototype/arpartment_managing/security/SecurityConfig.java
+++ backend/src/main/java/com/prototype/arpartment_managing/security/SecurityConfig.java
@@ -111,7 +111,7 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(List.of("http://localhost:5000"));
+                configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5000"));
                 configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
                 configuration.setExposedHeaders(List.of("Authorization"));
